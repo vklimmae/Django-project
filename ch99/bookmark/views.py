f84de0c5daba2822151a9f088453521900d8f69e
vklimmae <vanessakkk@naver.com>
2020-07-14 17:21:06 +0900

    북마크 기능

---
+++ ch99/bookmark/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
