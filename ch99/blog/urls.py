2f5f06d823e5db778360ab506946578975cf3a5f
vklimmae <vanessakkk@naver.com>
2020-07-14 17:23:45 +0900

    북마크 기능

---
+++ ch99/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from blog import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.PostLV.as_view(), name='index'),
+    path('post/',views.PostLV.as_view(), name='post_list'),
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+]
